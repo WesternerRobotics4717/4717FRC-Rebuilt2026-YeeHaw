8f8bbb33312c2dc468ea84aea80283f94f2cfe24
QuadrollPlayz <themightyquinnoliver@gmail.com>
2026-03-20 09:10:35 -0500

    Fixed .gitignore (removed build + junk files)

--- build/spotless/spotlessJava/src/main/java/frc/robot/subsystems/Shooter/Hood.java
+++
@@ -1,3 +0,0 @@
-package frc.robot.subsystems.Shooter;
-
-public class Hood {}
--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "4717FRC-Rebuilt2026-YeeHaw";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 14;
-  public static final String GIT_SHA = "9b6f76d4341b58e03fe59ee1177aaa5f269e77b4";
-  public static final String GIT_DATE = "2026-03-19 13:24:50 EDT";
+  public static final int GIT_REVISION = 15;
+  public static final String GIT_SHA = "c27942e4071f1d5bb3efb2f2da0683414dc80113";
+  public static final String GIT_DATE = "2026-03-19 20:33:49 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-19 20:27:57 EDT";
-  public static final long BUILD_UNIX_TIME = 1773966477674L;
+  public static final String BUILD_DATE = "2026-03-20 00:45:30 EDT";
+  public static final long BUILD_UNIX_TIME = 1773981930557L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
